6789b0606164a51393dd440f0cc5fca6463335e4
tianjun <tianjun@odboy.cn>
2022-09-29 10:48:25 +0800

    init...

---
+++ src/store/modules/d2admin/modules/account.js
@@ -0,0 +1,25 @@
+export default {
+  namespaced: true,
+  actions: {
+    /**
+     * @description 用户登录后从持久化数据加载一系列的设置
+     * @param {Object} context
+     */
+    async load ({ dispatch }) {
+      // 加载用户名
+      await dispatch('d2admin/user/load', null, { root: true })
+      // 加载主题
+      await dispatch('d2admin/theme/load', null, { root: true })
+      // 加载页面过渡效果设置
+      await dispatch('d2admin/transition/load', null, { root: true })
+      // 持久化数据加载上次退出时的多页列表
+      await dispatch('d2admin/page/openedLoad', null, { root: true })
+      // 持久化数据加载侧边栏配置
+      await dispatch('d2admin/menu/asideLoad', null, { root: true })
+      // 持久化数据加载全局尺寸
+      await dispatch('d2admin/size/load', null, { root: true })
+      // 持久化数据加载颜色设置
+      await dispatch('d2admin/color/load', null, { root: true })
+    }
+  }
+}
---
+++ src/views/system/index/page.vue
@@ -0,0 +1,17 @@
+<template>
+  <d2-container class="page">
+    开放资源首页
+  </d2-container>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+}
+</style>
